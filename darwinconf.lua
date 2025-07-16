211995d7fed4f234fdc8115694107e97fd1c6b72
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-07-15 23:48:33 -0300

    created standard build

---
+++ darwinconf.lua
@@ -0,0 +1,28 @@
+local CONCAT_PATH = true 
+
+function build_api()
+    local files = darwin.dtw.list_files_recursively("api",CONCAT_PATH)
+    local content = ""
+    for i =1,#files do 
+        content = content.."\n"..darwin.dtw.load_file(files[i]) 
+    end  
+    return content
+end
+
+function  build_cli(api_content)
+    local files = darwin.dtw.list_files_recursively("cli",CONCAT_PATH)
+    local content = api_content
+    for i =1,#files do 
+        content = content.."\n"..darwin.dtw.load_file(files[i]) 
+    end     
+    return content
+end
+
+function  main()
+    local api_content = build_api()
+    local cli_content = build_cli(api_content)
+    darwin.dtw.write_file("release/api.lua", api_content)
+    darwin.dtw.write_file("release/cli.lua", cli_content)
+end
+
+main()
